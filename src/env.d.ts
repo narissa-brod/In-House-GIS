853ae7eebd2ee8ea3087ecc42e067d0621f71f92
Narissa Broderick <narissa@colewest.com>
2025-10-02 10:23:52 -0600

    Set up new folder destination

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,10 @@
 /// <reference types="vite/client" />
-/// <reference types="google.maps" />
 
 interface ImportMetaEnv {
-  readonly VITE_AIRTABLE_BASE?: string;
-  readonly VITE_AIRTABLE_TABLE_ID?: string;
-  readonly VITE_AIRTABLE_VIEW?: string; // view NAME for Airtable API
-  readonly VITE_AIRTABLE_VIEW_ID?: string; // internal view id (optional)
-  readonly VITE_AIRTABLE_TOKEN?: string;
-  readonly VITE_GOOGLE_MAPS_KEY?: string;
+  readonly VITE_AIRTABLE_BASE: string;
+  readonly VITE_AIRTABLE_TABLE_ID: string;
+  readonly VITE_AIRTABLE_VIEW_ID?: string;
+  readonly VITE_GOOGLE_MAPS_KEY: string;
 }
 
 interface ImportMeta {
